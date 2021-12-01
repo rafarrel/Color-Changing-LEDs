6de20ca718cdd9a723e928cfe16250038a713d3a
Alex Farrell <rfarrel2@uvm.edu>
2021-12-01 13:21:47 -0500

    Set up code skeleton

--- system_controller/system_controller.ino
+++ system_controller/system_controller.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+ * TODO: MAKE BETTER HEADER
+ *  
+ * Embedded Programming C Final Project 
+ * Alex Farrell, Maegan Beckage
+ * 
+ * Term legend:
+ *    1) PWM -> Pulse Width Modulation
+ */
 
+/**************************************************************/
+
+// Pins (change placeholder values)
+const int RED   = 0;
+const int GREEN = 1;
+const int BLUE  = 2;
+
+/**************************************************************/
+void setup() {
+  // Initialize output pins for PWM
+  pinMode(RED  , OUTPUT)
+  pinMode(GREEN, OUTPUT)
+  pinMode(BLUE , OUTPUT)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Testing code for PWM
+  //          LED  , "Brightness"
+  analogWrite(RED  , 32 );
+  analogWrite(GREEN, 78 );
+  analogWrite(BLUE , 105);
 }
