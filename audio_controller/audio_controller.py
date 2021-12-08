d28fbfb1d05dceb4a09cf2e16ca765f95753170a
Alex Farrell <rfarrel2@uvm.edu>
2021-12-08 13:49:01 -0500

    Set up basic audio playback and testing

--- audio_controller/audio_controller.py
+++ audio_controller/audio_controller.py
@@ -3,14 +3,41 @@
                         of any audio being played and writing a description of it
                         to the Arduino.
 """
+import pyaudio
 import serial
+import wave
+import time
 
+
+# Basic testing function to play the audio from a wav file.
+def play_audio_file(filename):
+    try:
+        audio_wav   = wave.open(filename, 'rb')
+        audio_data  = audio_wav.readframes(audio_wav.getnframes())
+        audio_audio = pyaudio.PyAudio()
+        
+        stream_out = audio_audio.open(
+            format=audio_audio.get_format_from_width(audio_wav.getsampwidth()),
+            channels=audio_wav.getnchannels(),
+            rate=audio_wav.getframerate(),
+            input=False,
+            output=True
+        )
+
+        stream_out.start_stream()
+        stream_out.write(audio_data)
+        stream_out.stop_stream()
+        stream_out.close()
+        audio_audio.terminate()
+    except:
+        print("ERROR: AUDIO FILE NOT FOUND.")
 
 # Arduino connection settings
-arduino = serial.Serial(port='COM6')
+#arduino = serial.Serial(port='COM6')
 
 # Perform processing
 if __name__ == '__main__':
-    while True:
-        testInput = input("Enter Testing String: ")
-        arduino.write(bytes(testInput, 'utf-8'))
+    play_audio_file('../test_songs/Galantis - No Money.wav')
+    #while True:
+    #    testInput = input("Enter Testing String: ")
+    #    arduino.write(bytes(testInput, 'utf-8'))
