db8c5b0661647850968c02a8a69342d5b2a87416
Alex Farrell <rfarrel2@uvm.edu>
2021-12-09 17:15:08 -0500

    Working prototype with all colors implemented

--- audio_controller/audio_controller_functions.py
+++ audio_controller/audio_controller_functions.py
@@ -7,7 +7,7 @@ import audio_controller_settings as settings
 # LED colors should only be changed when the pitch changes 
 # significantly. There needs to be a mini delay between calls
 # to process the audio to give the arduino time to light the LEDs. 
-delay_count = 0
+delay_count = settings.MAX_DELAY
 last_color  = settings.OFF
 
 def change_LED_color(color):
@@ -24,14 +24,22 @@ def process_audio(data):
     audio_data = np.frombuffer(data, dtype=np.int16)
 
     # Root-mean-square of the audio data (power of the audio signal)
-    audio_rms  = audioop.rms(audio_data, 2) 
-    print(audio_rms)
+    audio_rms = audioop.rms(audio_data, 2) / 1000
 
-    # TESTING
-    if audio_rms >= 6000:
+    # Change colors based on audio_rms thresholds.
+    if audio_rms <= settings.REALLY_SOFT:
+        change_LED_color(settings.CYAN)
+    elif audio_rms <= settings.SOFT:
+        change_LED_color(settings.PURPLE)
+    elif audio_rms <= settings.MEDIUM:
+        change_LED_color(settings.GREEN)
+    elif audio_rms <= settings.LOUD:
         change_LED_color(settings.ORANGE)
-    else:
-        change_LED_color(settings.RED)
+    elif audio_rms > settings.REALLY_LOUD:
+        change_LED_color(settings.RED)         
+
+    # DISPLAY FOR TESTING
+    print(audio_rms)
 
 def callback(in_data, frame_count, time_info, status):
     """Callback function to allow instant processing of audio. Called continuously 
@@ -41,7 +49,7 @@ def callback(in_data, frame_count, time_info, status):
     # Raw audio data
     data = settings.audio_wav_file.readframes(frame_count)
 
-    if delay_count == 50:
+    if delay_count == settings.MAX_DELAY:
         process_audio(data)
         delay_count = 0
     else:
--- audio_controller/audio_controller_settings.py
+++ audio_controller/audio_controller_settings.py
@@ -10,6 +10,16 @@ arduino = serial.Serial(port='COM6')
 audio_wav_file  = wave.open('../test_songs/Galantis - No Money.wav', 'rb')
 audio_processor = pyaudio.PyAudio()
 
+# Delay count required to change LED colors
+MAX_DELAY = 50
+
+# Sound codes
+REALLY_SOFT = 4
+SOFT        = 6
+MEDIUM      = 8
+LOUD        = 10
+REALLY_LOUD = LOUD
+
 # Color codes
 CYAN   = 0
 PURPLE = 1
