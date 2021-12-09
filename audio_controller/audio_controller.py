a0d1deaa5013f0f0c0074dbc5db3ae94f1d0ad52
Alex Farrell <rfarrel2@uvm.edu>
2021-12-09 13:33:55 -0500

    Prepared callback function for audio processing

--- audio_controller/audio_controller.py
+++ audio_controller/audio_controller.py
@@ -3,18 +3,37 @@
                         of any audio being played and writing a description of it
                         to the Arduino.
 """
-import audio_controller_settings as settings
+import time
+import audio_controller_functions as functions
+import audio_controller_settings  as settings
 
 
 # Perform processing
 if __name__ == '__main__':
-    settings.stream_out.start_stream()
+    # Delay so Arduino connection can be established.
+    print('Connecting to Arduino...')
+    time.sleep(2)
+    print('Connection established.')
 
-    while settings.stream_out.is_active():
-        testInput = input("Enter Testing String: ")
-        settings.arduino.write(bytes(testInput, 'utf-8'))
+    # Audio playback/output stream
+    stream_out = settings.audio_processor.open(
+                 format=settings.audio_processor.get_format_from_width(settings.audio_wav_file.getsampwidth()),
+               channels=settings.audio_wav_file.getnchannels(),
+                   rate=settings.audio_wav_file.getframerate(),
+                  input=False,
+                 output=True,
+        stream_callback=functions.callback
+    )
 
-    settings.stream_out.stop_stream()
-    settings.stream_out.close()
+    # Start stream
+    stream_out.start_stream()
+
+    ## Runs audio processing in the callback function ##
+    while stream_out.is_active():
+        pass
+
+    # Cleanup
+    stream_out.stop_stream()
+    stream_out.close()
     settings.audio_wav_file.close()
     settings.audio_processor.terminate()
--- audio_controller/audio_controller_functions.py
+++ audio_controller/audio_controller_functions.py
@@ -2,7 +2,17 @@ import pyaudio
 import audio_controller_settings as settings
 
 
+# LED colors should only be changed when the pitch changes 
+# significantly. 
+pitchChanged = True
+
 def callback(in_data, frame_count, time_info, status):
     """Callback function to allow instant processing of audio."""
+    global pitchChanged
     data = settings.audio_wav_file.readframes(frame_count)
+
+    if pitchChanged:
+        settings.arduino.write('Medium'.encode('utf-8'))
+        pitchChanged = False
+
     return (data, pyaudio.paContinue)
